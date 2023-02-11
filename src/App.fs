ea7b771ce20abe271f828a094dff0eb4c08a24d9
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-02-11 12:14:09 +0100

    feat: delayed incrementing

--- src/App.fs
+++ src/App.fs
@@ -4,39 +4,56 @@ open Elmish
 open Elmish.React
 open Feliz
 
-type State =
-    { Count: int }
+type State = { Count: int; Loading: bool }
 
 type Msg =
     | Increment
     | Decrement
+    | IncrementDelayed
 
-let init() =
-    { Count = 0 }
+let init () =
+    { Count = 0; Loading = false }, Cmd.none
 
-let update (msg: Msg) (state: State): State =
+let update msg state =
     match msg with
     | Increment ->
-        { state with Count = state.Count + 1 }
+        { state with
+            Count = state.Count + 1
+            Loading = false },
+        Cmd.none
+    | Decrement -> { state with Count = state.Count - 1 }, Cmd.none
+    | IncrementDelayed when state.Loading = true -> state, Cmd.none
+    | IncrementDelayed ->
+        let incrementDelayedCmd dispatch =
+            let delayedDispatch =
+                async {
+                    do! Async.Sleep 1000
+                    dispatch Increment
+                }
 
-    | Decrement ->
-        { state with Count = state.Count - 1 }
+            Async.StartImmediate delayedDispatch
+
+        { state with Loading = true }, Cmd.ofSub incrementDelayedCmd
 
 let render (state: State) (dispatch: Msg -> unit) =
-  Html.div [
-    Html.button [
-      prop.onClick (fun _ -> dispatch Increment)
-      prop.text "Increment"
-    ]
+    printfn "rendering"
+    let content =
+        if state.Loading then
+            Html.h1 "LOADING..."
+        else
+            Html.h1 state.Count
 
-    Html.button [
-      prop.onClick (fun _ -> dispatch Decrement)
-      prop.text "Decrement"
-    ]
+    Html.div
+        [ content
+          Html.button [ prop.onClick (fun _ -> dispatch Increment); prop.text "Increment" ]
 
-    Html.h1 state.Count
-  ]
+          Html.button [ prop.onClick (fun _ -> dispatch Decrement); prop.text "Decrement" ]
 
-Program.mkSimple init update render
+          Html.button
+              [ prop.onClick (fun _ -> dispatch IncrementDelayed)
+                prop.disabled state.Loading
+                prop.text "Increment Delayed" ] ]
+
+Program.mkProgram init update render
 |> Program.withReactSynchronous "elmish-app"
 |> Program.run
