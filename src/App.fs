b9000477b5d968b5844b0ff8cadd84e8969d6924
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2023-02-20 15:04:08 +0100

    feat: add async random number generator

--- src/App.fs
+++ src/App.fs
@@ -17,16 +17,29 @@ module Cmd =
 
         Cmd.ofSub sub
 
-type State = { Count: int; Loading: bool }
+type State =
+    { Count: int
+      Loading: bool
+      RandomNumber: double }
+
+type AsyncOperationStatus<'t> =
+    | Started
+    | Finished of 't
 
 type Msg =
     | Increment
     | Decrement
     | IncrementDelayed
     | DecrementDelayed
+    | GenerateRandomNumber of AsyncOperationStatus<double>
+
+let rnd = System.Random()
 
 let init () =
-    { Count = 0; Loading = false }, Cmd.none
+    { Count = 0
+      Loading = false
+      RandomNumber = 0 },
+    Cmd.none
 
 
 let delayedMsg msg =
@@ -35,6 +48,12 @@ let delayedMsg msg =
         return msg
     }
 
+let generateRandomNumber () =
+    async {
+        let nextDouble = rnd.NextDouble()
+        do! Async.Sleep 200
+        return GenerateRandomNumber(Finished nextDouble)
+    }
 
 let update msg state =
     match msg with
@@ -48,6 +67,8 @@ let update msg state =
     | IncrementDelayed -> { state with Loading = true }, Cmd.fromAsync (delayedMsg Increment)
     | DecrementDelayed when state.Loading = true -> state, Cmd.none
     | DecrementDelayed -> state, Cmd.fromAsync (delayedMsg Decrement)
+    | GenerateRandomNumber Started -> state, generateRandomNumber () |> Cmd.fromAsync
+    | GenerateRandomNumber(Finished x) -> { state with RandomNumber = x }, Cmd.none
 
 let render (state: State) (dispatch: Msg -> unit) =
     printfn "rendering"
@@ -59,7 +80,11 @@ let render (state: State) (dispatch: Msg -> unit) =
             Html.h1 state.Count
 
     Html.div
-        [ content
+        [ Html.button
+              [ prop.onClick (fun _ -> dispatch (GenerateRandomNumber Started))
+                prop.text "Randomize me" ]
+          Html.h1 state.RandomNumber
+          content
           Html.button [ prop.onClick (fun _ -> dispatch Increment); prop.text "Increment" ]
 
           Html.button [ prop.onClick (fun _ -> dispatch Decrement); prop.text "Decrement" ]
